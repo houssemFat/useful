6625cccb119c602c1749db3a4f4371985cb68f41
houssemFat <fathallah.houssem@gmail.com>
2015-11-25 18:33:33 +0100

    Update heroku_acpo.sh
    enhance repetitive action by reply actions

--- heroku_acpo.sh
+++ heroku_acpo.sh
@@ -1,10 +1,27 @@
 #!/bin/bash
 # init
-# defaul folder /c/Users/houssem.fathallah/work/training/quickerio/
+# defaul folder 	/c/Users/houssem.fathallah/work/training/quickerio/
+entties="$*"
 
-function pause(){
-   read -p "$*"
+entties_count="$#"
+
+working_folder = ""
+
+function redo(){
+   echo "q to quit, r : reply"
+   read action
+   echo "$action"
+   if [ "$action" == "r" ]; then 
+		init
+	else 
+		echo "nothing to do, press [enter] to quit "
+		quit
+	fi
 }
+function quit (){
+	read -p ''
+}
+
 function dowrok (){
 	#!/bin/bash
 	# bash trap command
@@ -13,16 +30,22 @@ function dowrok (){
 	git commit -m 'message !'
 	git push heroku master 
 	heroku open
-	pause 'Press [Enter] key to continue...'
+	redo
+	
 }
-if [ "$#" -eq 0 ]; then 
-	echo "give me where your heroku folder"
-	read folder
-	echo "$folder"
-	cd "$folder"
-	dowrok 
-else 
-	dowrok
-	# ...
-	# call it
-fi 
+function init(){
+	echo "start init"
+	#echo "$working_folder"
+	#echo ${#working_folder}
+	if  [ ${#working_folder} -eq 0 ] &&  [ "$entties_count" -eq 0 ]   ; then 
+		echo "no foder specified , give me where your heroku folder"
+		read working_folder
+		echo "start working in >>> $working_folder"
+		cd "$working_folder"
+		dowrok 
+	else 
+		dowrok
+	fi
+}
+
+init 
